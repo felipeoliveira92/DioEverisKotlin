7a6b4373fd3f0c0e75332787c9772dfe03c65fb0
felipeoliveira92 <feliperabelo.oliveira@gmail.com>
2021-04-24 16:18:39 -0300

    Desafio 1

---
+++ Desafio 1/01SomaSimples.kt
@@ -0,0 +1,15 @@
+package `Desafio 1`
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    //input receber dados de entrada
+    val input = Scanner(System.`in`)
+    //Atribuir valor de entrada a variavel A = Valor Inteiro(nexInt())
+    val a = input.nextInt()
+    //Atribuir valor de entrada a variavel B = Valor Inteiro(nexInt())
+    val b = input.nextInt()
+    // com os valores atribuido as variaveis se realiza a soma de A + B
+    //e posteriomente o print na tela ou saida de dados
+    println("SOMA = "+ ( a  + b   ))
+}
